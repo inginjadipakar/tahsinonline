42b5419342b9039871c2181b73a8470ba35171bb
TahsinKu Admin <tahsinku@example.com>
2025-12-23 16:50:51 +0700

    Fix null user error in subscriptions index with optional chaining

--- resources/views/admin/subscriptions/index.blade.php
+++ resources/views/admin/subscriptions/index.blade.php
@@ -30,7 +30,7 @@
                                 @forelse ($subscriptions as $subscription)
                                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                         <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {{ $subscription->user->name }}
+                                            {{ $subscription->user?->name ?? 'User Deleted' }}
                                         </td>
                                         <td class="px-6 py-4">{{ $subscription->start_date }}</td>
                                         <td class="px-6 py-4">{{ $subscription->end_date }}</td>
